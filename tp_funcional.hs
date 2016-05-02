abace1a73f553a21cc1b366ce0013cf6b8e78708
gamba14 <gambirassi.a@gmail.com>
2016-05-02 09:56:22 -0300

    tp_funcional
    fijaos que os faltan un puñado de pendejadas!

---
+++ tp_funcional.hs
@@ -0,0 +1,15 @@
+import Data.List
+import Data.Char
+data Archivo = Archivo {nombre :: String , contenido :: String} deriving (Show, Eq)
+
+unTpGrupal :: Archivo
+
+unTpGrupal = Archivo "tpGrupal.hs" "listaLarga :: [a] -> Bool \n listaLarga = (>9) . length \n    \n hola " 
+
+tamanioArchivo archivo = length(contenido archivo)*8 
+esVacio archivo = length(contenido archivo) == 0
+cantLineas archivo = length(lines(contenido archivo))
+--esLineaEnBlanco archivo =  
+esHs archivo = drop (length(nombre archivo)-3) (nombre archivo) == ".hs"
+renombrarArchivo archivo nuevo = 	
+
