1f11c312e5ecb4dde36ca4fd3875658631e2a35a
EmmanuelPL <emmanuel.posadasl@gmail.com>
2016-05-03 18:50:38 -0300

    Implementado renombrar archivo

--- tp_funcional.hs
+++ tp_funcional.hs
@@ -7,9 +7,14 @@ unTpGrupal :: Archivo
 unTpGrupal = Archivo "tpGrupal.hs" "listaLarga :: [a] -> Bool \n listaLarga = (>9) . length \n    \n hola " 
 
 tamanioArchivo archivo = length(contenido archivo)*8 
+
 esVacio archivo = length(contenido archivo) == 0
+
 cantLineas archivo = length(lines(contenido archivo))
+
 --esLineaEnBlanco archivo =  
+
 esHs archivo = drop (length(nombre archivo)-3) (nombre archivo) == ".hs"
-renombrarArchivo archivo nuevo = 	
+
+renombrarArchivo archivo nuevo = Archivo nuevo (contenido archivo) 
 
